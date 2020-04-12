d4cf9583b6fd628b1fae9054ea41c06ab1a47fd7
charlelucky <xingbod@gmail.com>
2020-04-12 20:47:30 +0800

    prepare triplet data

--- data/convert_train_tfrecord_4_tripletloss.py
+++ data/convert_train_tfrecord_4_tripletloss.py
@@ -73,14 +73,14 @@ def main(_):
     allsubdir = [os.path.join(dataset_path, o) for o in os.listdir(dataset_path)
                  if os.path.isdir(os.path.join(dataset_path, o))]
     path_ds = tf.data.Dataset.from_tensor_slices(allsubdir)
-    ds = path_ds.interleave(lambda x: processOneDir4(x), cycle_length=5751,
+    ds = path_ds.interleave(lambda x: processOneDir4(x), cycle_length=85742,
                             block_length=2,
                             num_parallel_calls=4).batch(4, True).map(pair_parser, -1).batch(1, True).map(
         generateTriplet, -1)
     iters = iter(ds)
     iters = iter(ds)
     with tf.io.TFRecordWriter("triplets_ds.tfrecord") as writer:
-        for i in tqdm.tqdm(os.listdir(dataset_path)):
+        for i in tqdm.tqdm(500000):
             imgs, label = next(iters)
             tf_example = make_example(source_id=label,
                                       img_path=[imgs[0].numpy()[0], imgs[1].numpy()[0], imgs[2].numpy()[0]])
