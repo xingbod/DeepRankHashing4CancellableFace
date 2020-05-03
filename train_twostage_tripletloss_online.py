7dc863edfabe5f09f36b22fbd31e358eb0126212
charlelucky <xingbod@gmail.com>
2020-05-03 18:10:02 +0800

    update margin loss

--- train_twostage_tripletloss_online.py
+++ train_twostage_tripletloss_online.py
@@ -135,19 +135,6 @@ def main(_):
                 elif cfg['loss_fun'] == 'ms_loss':
                     pred_loss = ms_loss.ms_loss(labels, logist,ms_mining=True)
                 elif cfg['loss_fun'] == 'margin_loss':
-                    if steps ==1:
-                        beta_0 = 1.2  # initial learnable beta value
-                        number_identities = 85742  # for mnist
-                        params = {
-                            'alpha': 0.2,
-                            'nu': 0.,
-                            'cutoff': 0.5,
-                            'add_summary': True,
-                        }
-                        betas = tf.constant(
-                        #     name =  'beta_margins',
-                            # trainable = True,initial_value=
-                            beta_0 * tf.ones([number_identities]))
                     pred_loss = margin_loss.margin_loss(labels, logist,betas,params)
                 elif cfg['loss_fun'] == 'batch_all_arc_triplet_loss':
                     pred_loss, _ ,_= arcface_pair_loss.batch_all_triplet_arcloss(labels, logist, arc_margin=cfg['triplet_margin'], scala=100)
