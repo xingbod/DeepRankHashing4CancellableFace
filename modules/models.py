ff4dc37740965d1196fdf6d4c1897df2b3b1cc7d
charlelucky <xingbod@gmail.com>
2020-05-20 00:20:58 +0800

    update 3 layers

--- modules/models.py
+++ modules/models.py
@@ -221,12 +221,11 @@ def IoMFaceModelFromArFace3(size=None, channels=3, arcmodel=None, name='IoMface_
         x = PermLayer(permKey)(x)  # permutation before project to IoM hash code
     # here I add one extra hidden layer
     x = Dense(1024, kernel_regularizer=_regularizer(w_decay))(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = Dropout(rate=0.5)(x)
     x = Dense(512, kernel_regularizer=_regularizer(w_decay))(x)
     x = BatchNormalization()(x)
     x = Dropout(rate=0.5)(x)
-    x = Flatten()(x)
     x = Dense(cfg['m'] * cfg['q'], kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=1, seed=None),name="IoMProjection")(x)  # extra connection layer
     logist = IoMHead(m=cfg['m'], q=cfg['q'], isTraining=training)(x)  # loss need to change
     return Model(inputs, logist, name=name)
