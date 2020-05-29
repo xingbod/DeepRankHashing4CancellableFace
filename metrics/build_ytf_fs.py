8aebd2bb8ece854d6e81ff9fdb6cf41105a424cb
charlelucky <xingbod@gmail.com>
2020-05-29 12:13:56 +0800

    update

--- metrics/build_ytf_fs.py
+++ metrics/build_ytf_fs.py
@@ -2,29 +2,7 @@ import os
 import pathlib
 import random
 from shutil import copy, rmtree, copytree, copy2
-import tensorflow as tf
-# DATASET_NAME = 'facescrub'
-DATASET_NAME = 'youtubeface'
-
-if DATASET_NAME == 'youtubeface':
-    TRAIN_SET_PATH = '/your/file/dir/to/youtubeface_train_set'
-    TEST_SET_PATH = '/your/file/dir/to/youtubeface_test_set'
-    NB_CLASSES = 1595
-else:
-    TRAIN_SET_PATH = '/your/file/dir/to/facescrub_train_set'
-    TEST_SET_PATH = '/your/file/dir/to/facescrub_test_set'
-    NB_CLASSES = 530
-
-WEIGHTS_SAVE_PATH = 'you/file/dir/to/weights'
-WEIGHTS_FILE_NAME = 'best/weights/dir/to/load'
-
-NB_EPOCHS = 200
-HASH_NUM = 48
-SPLIT_NUM = 4
-TOP_K = 50
-
-IMAGE_WIDTH = 112
-IMAGE_HEIGHT = 112
+import tqdm
 
 
 def check_path_valid(path):
@@ -45,7 +23,8 @@ SPLIT_WEIGHTS_INTRA_ID = (
 '''
 def splitDS(ds_path, save_dir, SPLIT_WEIGHTS=[40,5], ds='ytf'):
     dir_list = [dI for dI in os.listdir(ds_path) if os.path.isdir(os.path.join(ds_path, dI))]
-    for dir_name in dir_list:
+    cnt = 0;
+    for dir_name in tqdm.tqdm(dir_list):
         data_dir = pathlib.Path(os.path.join(ds_path, dir_name))
         if ds == 'ytf':
             pic_list = list(data_dir.glob('*/*.jpg'))
@@ -60,13 +39,14 @@ def splitDS(ds_path, save_dir, SPLIT_WEIGHTS=[40,5], ds='ytf'):
             print(data_dir, " has not enough samples!",len(pic_list))
         train_file_list = random.sample(pic_list[SPLIT_WEIGHTS[1]:], k=min(SPLIT_WEIGHTS[0],len(pic_list)-SPLIT_WEIGHTS[1]))
         for images in train_file_list:
-            dst_dir = os.path.join(save_dir + '/train_gallery', dir_name)
+            dst_dir = os.path.join(save_dir + '/train_gallery', cnt)
             check_folder(dst_dir)
             copy(images, dst_dir)
         for images in test_file_list:
-            dst_dir = os.path.join(save_dir + '/test', dir_name)
+            dst_dir = os.path.join(save_dir + '/test', cnt)
             check_folder(dst_dir)
             copy(images, dst_dir)
+        cnt = cnt+1
 
 if __name__ == '__main__':
     ds_path = './data/test_dataset/facescrub_images_112x112/112x112'
--- train_twostage2.py
+++ train_twostage2.py
@@ -143,10 +143,12 @@ def main(_):
                 elif cfg['loss_fun'] == 'triplet_sampling':
                     beta_0 = 1.2
                 quanti_loss = tf.cast(loss_fn_quanti(logist),tf.float64)
-                if cfg['bin_lut_loss']:
+                if cfg['bin_lut_loss'] == 'tanh':
                     bin_loss = bin_LUT_loss.binary_loss_LUT(labels, logist) * 0.001
+                elif cfg['bin_lut_loss'] == 'sigmoid':
+                    bin_loss = bin_LUT_loss.binary_loss_LUT_sigmoid(labels, logist) * 0.001
                 else:
-                    bin_loss = tf.constant(0.0,tf.float64)
+                    bin_loss = tf.constant(0.0, tf.float64)
                 total_loss = pred_loss + reg_loss * 0.5 + quanti_loss * 0.5 + bin_loss
 
             grads = tape.gradient(total_loss, model.trainable_variables)
