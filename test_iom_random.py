378487483fdece1e252d5d92e10259665fbe8cd4
charlelucky <xingbod@gmail.com>
2020-06-17 20:20:02 +0800

    update iom arch

--- test_iom_random.py
+++ test_iom_random.py
@@ -135,9 +135,9 @@ def callMe():
                 text_file.write(log_str2)
 
         evl(0)  # no LUT
-        evl(4)
-        evl(8)
-        evl(16)
+        # evl(4)
+        # evl(8)
+        # evl(16)
 
     # def main(_argv):
     #     for m in [32, 64, 128, 256, 512]:
@@ -165,21 +165,19 @@ def callMe():
 # mycfg['m'] = 512
 # mycfg['q'] = 8
 # callMe()
-for trail in [1, 2, 3]:
-    for m in [32, 64, 128, 256, 512]:
-        for q in [2, 4, 8, 16]:
-            print(m, q, '****')
-            mycfg['m'] = m
-            mycfg['q'] = q
-            callMe()
+for m in [256, 512]:
+    for q in [2, 4, 8, 16]:
+        print(m, q, '****')
+        mycfg['m'] = m
+        mycfg['q'] = q
+        callMe()
 
-for trail in [1, 2, 3]:
-    for m in [1024, 2048]:
-        for q in [2, 4]:
-            print(m, q, '****')
-            mycfg['m'] = m
-            mycfg['q'] = q
-            callMe()
+for m in [1024, 2048]:
+    for q in [2, 4]:
+        print(m, q, '****')
+        mycfg['m'] = m
+        mycfg['q'] = q
+        callMe()
 # for m in [2048]:
 #     for q in [2]:
 #         print(m,q,'****')
