d189bc8637ec3928b544a2d0741cc4133c8e2518
charlelucky <xingbod@gmail.com>
2020-07-14 00:46:54 +0800

    using T / beta

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import cv2
 import os
 import numpy as np
 import tensorflow as tf
+import csv
 
 from modules.evaluations import get_val_data, perform_val,perform_val_yts
 from modules.models import ArcFaceModel
@@ -65,6 +66,10 @@ def main(_argv):
             cfg['embd_shape'], cfg['batch_size'], model, lfw, lfw_issame,
             is_ccrop=cfg['is_ccrop'], cfg=cfg)
         print("    acc {:.4f}, th: {:.2f}, auc {:.4f}, EER {:.4f}".format(acc_lfw, best_th_lfw, auc_lfw, eer_lfw))
+
+        with open('embeddings/' + cfg['sub_name'] + '_embeddings_orig_lfw.csv', 'w', newline='') as file:
+            writer = csv.writer(file, escapechar='/', quoting=csv.QUOTE_NONE)
+            writer.writerows(embeddings_lfw)
 
         print("[*] Perform Evaluation on AgeDB30...")
         acc_agedb30, best_th_agedb30, auc_agedb30, eer_agedb30, embeddings_agedb30 = perform_val(
