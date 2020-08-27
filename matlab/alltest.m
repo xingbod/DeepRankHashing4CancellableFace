dcb5044d427d8e49bba44049d2b25be295cac95f
charleslucky <1214176716qq>
2020-08-27 14:59:40 +0800

    matlab testing modify a little bit

--- matlab/alltest.m
+++ matlab/alltest.m
@@ -27,25 +27,25 @@
 % enroll_query_orig_fusion('ResNet50_lfw_feat.csv','InceptionResNetV2_lfw_feat.csv','ResNet50_lfw_name.txt')
 % enroll_query_orig_fusion('ResNet50_lfw_feat.csv','lresnet100e_ir_lfw_feat.csv','ResNet50_lfw_name.txt')
 % enroll_query_orig_fusion('lresnet100e_ir_lfw_feat.csv','InceptionResNetV2_lfw_feat.csv','ResNet50_lfw_name.txt')
-
-for m = [64 128 256 512]
-    for q = [2 4 8 16]
-        enroll_query_hashing("ResNet50_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'ResNet50_lfw_name.txt')
-    end
-end
-
-for m = [64 128 256 512]
-    for q = [2 4 8 16]
-        enroll_query_hashing("lresnet100e_ir_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'ResNet50_lfw_name.txt')
-    end
-end
-
-for m = [64 128 256 512]
-    for q = [2 4 8 16]
-        enroll_query_hashing("InceptionResNetV2_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'ResNet50_lfw_name.txt')
-    end
-end
-%%
+% 
+% for m = [64 128 256 512]
+%     for q = [2 4 8 16]
+%         enroll_query_hashing("ResNet50_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'ResNet50_lfw_name.txt')
+%     end
+% end
+% 
+% for m = [64 128 256 512]
+%     for q = [2 4 8 16]
+%         enroll_query_hashing("lresnet100e_ir_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'ResNet50_lfw_name.txt')
+%     end
+% end
+% 
+% for m = [64 128 256 512]
+%     for q = [2 4 8 16]
+%         enroll_query_hashing("InceptionResNetV2_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'ResNet50_lfw_name.txt')
+%     end
+% end
+% %%
 for m = [64 128 256 512]
     for q = [2 4 8 16]
         enroll_query_iom_id("ResNet50_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'ResNet50_lfw_name.txt')
@@ -64,6 +64,10 @@ for m = [64 128 256 512]
     end
 end
 
-
+for m = [64 128 256 512]
+    for q = [2 4 8 16]
+        enroll_query_iom_fusion_id("ResNet50_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv","InceptionResNetV2_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'ResNet50_lfw_name.txt')
+    end
+end
 % 
 % enroll_query_iom_fusion(hashcode_path,hashcode_path2,filename_path)
