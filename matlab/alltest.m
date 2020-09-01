934d21226bc47a27b1bbb6488ccf6718a4684cc7
charleslucky <1214176716qq>
2020-09-01 15:36:46 +0800

    matlab

--- matlab/alltest.m
+++ matlab/alltest.m
@@ -51,7 +51,7 @@ ds = "LFW";
 remark = "deep_orig_feat";
 enroll_query_search("embeddings_0831/ResNet50_lfw_feat.csv", 'embeddings/ResNet50_lfw_name.txt',measure,"LFW",remark)
 enroll_query_search("embeddings_0831/ResNet50_VGG2_feat.csv", 'embeddings_0831/ResNet50_VGG2_name.txt',measure,"VGG2",remark)
-enroll_query_search("embeddings_0831/ResNet50_ijbc_feat.csv", 'embeddings_0831/ResNet50_ijbc_name.txt',measure,"IJBC",remark)
+% enroll_query_search("embeddings_0831/ResNet50_ijbc_feat.csv", 'embeddings_0831/ResNet50_ijbc_name.txt',measure,"IJBC",remark)
 
 remark = "random_iom";
 measure = "Hamming";
@@ -60,7 +60,7 @@ for m = [64 128 256 512]
     for q = [8]
         enroll_query_search("embeddings_0831/ResNet50_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings/ResNet50_lfw_name.txt',measure,"LFW",remark)
         enroll_query_search("embeddings_0831/ResNet50_VGG2_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_VGG2_name_64x8.txt',measure,"VGG2",remark)
-        enroll_query_search("embeddings_0831/ResNet50_ijbc_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_ijbc_name_64x8.txt',measure,"IJBC",remark)
+%         enroll_query_search("embeddings_0831/ResNet50_ijbc_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_ijbc_name_64x8.txt',measure,"IJBC",remark)
     end
 end
 
@@ -68,7 +68,7 @@ for m = [512]
     for q = [2 4 16]
         enroll_query_search("embeddings_0831/ResNet50_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings/ResNet50_lfw_name.txt',measure,"LFW",remark)
         enroll_query_search("embeddings_0831/ResNet50_VGG2_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_VGG2_name_64x8.txt',measure,"VGG2",remark)
-        enroll_query_search("embeddings_0831/ResNet50_ijbc_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_ijbc_name_64x8.txt',measure,"IJBC",remark)
+%         enroll_query_search("embeddings_0831/ResNet50_ijbc_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_ijbc_name_64x8.txt',measure,"IJBC",remark)
     end
 end
 
@@ -79,7 +79,7 @@ for m = [64 128 256 512]
     for q = [8]
         enroll_query_iom_id("embeddings_0831/ResNet50_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings/ResNet50_lfw_name.txt',measure,"LFW",remark)
         enroll_query_iom_id("embeddings_0831/ResNet50_VGG2_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_VGG2_name_64x8.txt',measure,"VGG2",remark)
-        enroll_query_iom_id("embeddings_0831/ResNet50_ijbc_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_ijbc_name_64x8.txt',measure,"IJBC",remark)
+%         enroll_query_iom_id("embeddings_0831/ResNet50_ijbc_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_ijbc_name_64x8.txt',measure,"IJBC",remark)
     end
 end
 
@@ -87,7 +87,7 @@ for m = [512]
     for q = [2 4 16]
         enroll_query_iom_id("embeddings_0831/ResNet50_lfw_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings/ResNet50_lfw_name.txt',measure,"LFW",remark)
         enroll_query_iom_id("embeddings_0831/ResNet50_VGG2_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_VGG2_name_64x8.txt',measure,"VGG2",remark)
-        enroll_query_iom_id("embeddings_0831/ResNet50_ijbc_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_ijbc_name_64x8.txt',measure,"IJBC",remark)
+%         enroll_query_iom_id("embeddings_0831/ResNet50_ijbc_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/ResNet50_ijbc_name_64x8.txt',measure,"IJBC",remark)
     end
 end
 
