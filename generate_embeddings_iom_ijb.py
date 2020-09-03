34d38249525ba5542a684a5228c589dff744a09d
charleslucky <1214176716qq>
2020-09-03 10:44:10 +0800

    upload doc

--- generate_embeddings_iom_ijb.py
+++ generate_embeddings_iom_ijb.py
@@ -70,13 +70,12 @@ def main(_argv):
 
     dataset = load_data_from_dir('/media/Storage/facedata/ijbc_mtcnn_160/images/img', BATCH_SIZE=cfg['eval_batch_size'],
                                  img_ext='png', ds='IJBC')
-    feats1, names1, n = extractFeat(dataset, arcmodel, 512)
+    feats1, names1, n = extractFeat(dataset, arcmodel)
 
     dataset2 = load_data_from_dir('/media/Storage/facedata/ijbc_mtcnn_160/images/frames', BATCH_SIZE=cfg['eval_batch_size'],
                                  img_ext='png', ds='IJBC')
-    feats2, names2, n = extractFeat(dataset2, arcmodel)
-    feats = [feats1,feats2]
-    names = [names1, names2]
+    feats, names, n = extractFeatAppend(dataset2, arcmodel,feats1,names1)
+
     with open('embeddings_0831/' + cfg['backbone_type'] + '_ijbc_feat.csv',
               'w') as f:
         # using csv.writer method from CSV package
@@ -108,14 +107,13 @@ def main(_argv):
             dataset = load_data_from_dir('/media/Storage/facedata/ijbc_mtcnn_160/images/img',
                                          BATCH_SIZE=cfg['eval_batch_size'],
                                          img_ext='png', ds='IJBC')
-            feats1, names1, n = extractFeat(dataset, arcmodel, m)
+            feats1, names1, n = extractFeat(dataset, arcmodel)
             dataset2 = load_data_from_dir('/media/Storage/facedata/ijbc_mtcnn_160/images/frames',
                                           BATCH_SIZE=cfg['eval_batch_size'],
                                           img_ext='png', ds='IJBC')
-            feats2, names2, n = extractFeat(dataset2, arcmodel, m)
+            feats, names, n = extractFeatAppend(dataset2, arcmodel,feats1,names1)
 
-            feats = [feats1, feats2]
-            names = [names1, names2]
+
             with open('embeddings_0831/' + cfg['backbone_type'] + '_ijbc_feat_drIoM_' + str(cfg['m']) + 'x' + str(
                     cfg['q']) + '.csv',
                       'w') as f:
