33131f0a14f3102eb74e8d4b4064eb78c33d88c4
charleslucky <1214176716qq>
2020-09-04 20:06:58 +0800

    upload doc

--- matlab/run_single.m
+++ matlab/run_single.m
@@ -1,7 +1,7 @@
-function run_single(backbone,DS)
+function run_single(embpath,backbone,DS)
 
 % backbone = "ResNet50";%InceptionResNetV2
-% DS = "IJBC";run_single("ResNet50","LFW") run_single("ResNet50","VGG2")
+% DS = "IJBC";run_single("ResNet50","LFW") run_single("embeddings_0831","ResNet50","VGG2")
 
 
 if DS == "IJBC"
@@ -13,14 +13,14 @@ else DS == "LFW"
 end
 measure = "Euclidean";
 remark = "deep_orig_feat";
-enroll_query_search("embeddings_0831/"+backbone+"_"+embed+"_feat.csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat.csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
 
 remark = "random_iom";
 measure = "Hamming";
 for m = [64 128 256 512]
     for q = [8]
         tic
-        enroll_query_search("embeddings_0831/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+        enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
         toc
     end
 end
@@ -28,7 +28,7 @@ end
 for m = [512]
     for q = [2 4 16]
         tic
-        enroll_query_search("embeddings_0831/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+        enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
         toc
     end
 end
@@ -39,7 +39,7 @@ measure = "Hamming";
 for m = [64 128 256 512]
     for q = [8]
         tic
-        enroll_query_iom_id("embeddings_0831/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+        enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
         toc
     end
 end
@@ -47,7 +47,7 @@ end
 for m = [512]
     for q = [2 4 16]
         tic
-        enroll_query_iom_id("embeddings_0831/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+        enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
         toc
     end
 end
