d965947ee5843f6726b64f4a953634f59f004905
charleslucky <1214176716qq>
2020-09-09 23:01:23 +0800

    LUT

--- test_twostage_iom.py
+++ test_twostage_iom.py
@@ -154,11 +154,11 @@ def main(_argv):
 
         print(log_str2)
 
-    evl(0, 'Hamming')
-    evl(4, 'Hamming')
-    evl(3, 'Hamming')
+    # evl(0, 'Hamming')
+    # evl(4, 'Hamming')
+    # evl(3, 'Hamming')
     evl(2, 'Hamming')
-    evl(1, 'Hamming')
+    # evl(1, 'Hamming')
     # evl(int(math.log2(q)), 'Hamming')
     # evl(0, 'Euclidean')
     # # evl(q, 'Jaccard')
