5cf9d3159c1678cee81107f8ff07fe83c7bdb4af
charleslucky <xingbod@gmail.com>
2020-10-03 15:16:03 +0800

    update 1003 on InsightFace pre model

--- matlab/run_single.m
+++ matlab/run_single.m
@@ -19,42 +19,42 @@ measure = "Cosine";
 remark = "deep_orig_feat";
 enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat.csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
 
-% remark = "random_iom";
-% measure = "Hamming";
-% for m = [64 128 256 512]
-%     for q = [8]
-%         tic
-%         enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
-%         toc
-%     end
-% end
-% 
-% for m = [512]
-%     for q = [2 4 16]
-%         tic
-%         enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
-%         toc
-%     end
-% end
+remark = "random_iom";
+measure = "Hamming";
+for m = [64 128 256 512]
+    for q = [8]
+        tic
+        enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+        toc
+    end
+end
 
-%%
-% remark = "random_iom_identification";
-% measure = "Hamming";
-% for m = [64 128 256 512]
-%     for q = [8]
-%         tic
-%         enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
-%         toc
-%     end
-% end
-% 
-% for m = [512]
-%     for q = [2 4 16]
-%         tic
-%         enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
-%         toc
-%     end
-% end
+for m = [512]
+    for q = [2 4 16]
+        tic
+        enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+        toc
+    end
+end
+
+%
+remark = "random_iom_identification";
+measure = "Hamming";
+for m = [64 128 256 512]
+    for q = [8]
+        tic
+        enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+        toc
+    end
+end
+
+for m = [512]
+    for q = [2 4 16]
+        tic
+        enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+        toc
+    end
+end
 
 
 
