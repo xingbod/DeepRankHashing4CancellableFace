d2511fe70493409860789774dce44f3ee7ea9c27
charleslucky <xingbod@gmail.com>
2020-10-04 11:55:48 +0800

    update 1003 on InsightFace pre model

--- matlab/run_single.m
+++ matlab/run_single.m
@@ -21,7 +21,7 @@ enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat.csv", 'embeddings_0831
 
 remark = "random_iom";
 measure = "Hamming";
-for m = [64 128 256 512]
+for m = [ 512]%64 128 256
     for q = [8]
         tic
         enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
@@ -29,18 +29,18 @@ for m = [64 128 256 512]
     end
 end
 
-for m = [512]
-    for q = [2 4 16]
-        tic
-        enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
-        toc
-    end
-end
+% for m = [512]
+%     for q = [2 4 16]
+%         tic
+%         enroll_query_search(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+%         toc
+%     end
+% end
 
 %
 remark = "random_iom_identification";
 measure = "Hamming";
-for m = [64 128 256 512]
+for m = [512]
     for q = [8]
         tic
         enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
@@ -48,13 +48,13 @@ for m = [64 128 256 512]
     end
 end
 
-for m = [512]
-    for q = [2 4 16]
-        tic
-        enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
-        toc
-    end
-end
+% for m = [512]
+%     for q = [2 4 16]
+%         tic
+%         enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_drIoM_"+num2str(m)+"x"+num2str(q)+".csv", 'embeddings_0831/'+backbone+'_'+embed+'_name.txt',measure,DS,remark);
+%         toc
+%     end
+% end
 
 
 
