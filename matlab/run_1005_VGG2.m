d049a4e230b08331838ad371020281595ff0b6a0
charleslucky <xingbod@gmail.com>
2020-10-05 22:53:01 +0800

    update 1003 on InsightFace pre model

---
+++ matlab/run_1005_VGG2.m
@@ -0,0 +1,12 @@
+run_single_learning_all("embeddings_dl","Xception","VGG2")
+run_single_learning_all("embeddings_dl","ResNet50","VGG2")
+run_single_learning_all("embeddings_dl","InceptionResNetV2","VGG2")
+
+measure = "Hamming";
+remark = "fusion_learn_DIoMH";
+ds = "VGG2"
+enroll_query_iom_id_fusion("embeddings_dl/ResNet50_VGG2_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_VGG2_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_VGG2_name_dl_256x8.txt",measure,ds,remark)
+
+enroll_query_iom_id_fusion("embeddings_dl/ResNet50_VGG2_feat_dlIoM_512x8.csv","embeddings_dl/Xception_VGG2_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_VGG2_name_dl_256x8.txt",measure,ds,remark)
+
+enroll_query_iom_id_fusion("embeddings_dl/Xception_VGG2_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_VGG2_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_VGG2_name_dl_256x8.txt",measure,ds,remark)
---
+++ matlab/run_1005_ijbc.m
@@ -0,0 +1,12 @@
+run_single_learning_all("embeddings_dl","Xception","IJBC")
+run_single_learning_all("embeddings_dl","ResNet50","IJBC")
+run_single_learning_all("embeddings_dl","InceptionResNetV2","IJBC")
+
+measure = "Hamming";
+remark = "fusion_learn_DIoMH";
+ds = "IJBC"
+enroll_query_iom_id_fusion("embeddings_dl/ResNet50_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_IJBC_name_dl_256x8.txt",measure,ds,remark)
+
+enroll_query_iom_id_fusion("embeddings_dl/ResNet50_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/Xception_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_IJBC_name_dl_256x8.txt",measure,ds,remark)
+
+enroll_query_iom_id_fusion("embeddings_dl/Xception_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_IJBC_name_dl_256x8.txt",measure,ds,remark)
