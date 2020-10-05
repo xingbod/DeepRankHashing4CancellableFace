dc197b8adc58ea846ed402972058c32882e1345d
charleslucky <xingbod@gmail.com>
2020-10-05 23:05:21 +0800

    update 1003 on InsightFace pre model

--- matlab/run_single_learning_all.m
+++ matlab/run_single_learning_all.m
@@ -52,7 +52,7 @@ measure = "Hamming";
 for m = [64 128 256 512]
     for q = [8]
         tic
-        enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_dlIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'embeddings_dl/'+backbone+'_'+embed+'_name_'+num2str(m)+"x"+num2str(q)'.txt',measure,DS,remark);
+        enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_dlIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'embeddings_dl/'+backbone+'_'+embed+'_name_'+num2str(m)+"x"+num2str(q)+'.txt',measure,DS,remark);
         toc
     end
 end
@@ -60,7 +60,7 @@ end
 for m = [512]
     for q = [2 4 16]
         tic
-        enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_dlIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'embeddings_dl/'+backbone+'_'+embed+'_name_'+num2str(m)+"x"+num2str(q)'.txt',measure,DS,remark);
+        enroll_query_iom_id(embpath+"/"+backbone+"_"+embed+"_feat_dlIoM_"+num2str(m)+"x"+num2str(q)+".csv",  'embeddings_dl/'+backbone+'_'+embed+'_name_'+num2str(m)+"x"+num2str(q)+'.txt',measure,DS,remark);
         toc
     end
 end
