d4f0be5d0aba9781d97c69724528114865915b48
charleslucky <xingbod@gmail.com>
2020-10-05 11:36:40 +0800

    update 1003 on InsightFace pre model

--- generate_embeddings_ijbc2.py
+++ generate_embeddings_ijbc2.py
@@ -51,36 +51,11 @@ def main(_argv):
                             training=False,
                             # here equal false, just get the model without acrHead, to load the model trained by arcface
                             cfg=cfg)
-    if cfg['loss_fun'] == 'margin_loss':
-        model = IoMFaceModelFromArFaceMLossHead(size=cfg['input_size'],
-                                                arcmodel=arcmodel, training=False,
-                                                permKey=permKey, cfg=cfg)
-    else:
-        # here I add the extra IoM layer and head
-        if cfg['hidden_layer_remark'] == '1':
-            model = IoMFaceModelFromArFace(size=cfg['input_size'],
-                                           arcmodel=arcmodel, training=False,
-                                           permKey=permKey, cfg=cfg)
-        elif cfg['hidden_layer_remark'] == '2':
-            model = IoMFaceModelFromArFace2(size=cfg['input_size'],
-                                            arcmodel=arcmodel, training=False,
-                                            permKey=permKey, cfg=cfg)
-        elif cfg['hidden_layer_remark'] == '3':
-            model = IoMFaceModelFromArFace3(size=cfg['input_size'],
-                                            arcmodel=arcmodel, training=False,
-                                            permKey=permKey, cfg=cfg)
-        elif cfg['hidden_layer_remark'] == 'T':# 2 layers
-            model = IoMFaceModelFromArFace_T(size=cfg['input_size'],
-                                            arcmodel=arcmodel, training=False,
-                                            permKey=permKey, cfg=cfg)
-        elif cfg['hidden_layer_remark'] == 'T1':
-            model = IoMFaceModelFromArFace_T1(size=cfg['input_size'],
-                                            arcmodel=arcmodel, training=False,
-                                            permKey=permKey, cfg=cfg)
-        else:
-            model = IoMFaceModelFromArFace(size=cfg['input_size'],
-                                           arcmodel=arcmodel, training=False,
-                                           permKey=permKey, cfg=cfg)
+
+    model = IoMFaceModelFromArFace(size=cfg['input_size'],
+                                   arcmodel=arcmodel, training=False,
+                                   permKey=permKey, cfg=cfg)
+
     if FLAGS.ckpt_epoch == '':
         ckpt_path = tf.train.latest_checkpoint('./checkpoints/' + cfg['sub_name'])
     else:
@@ -90,10 +65,9 @@ def main(_argv):
         model.load_weights(ckpt_path)
     else:
         print("[*] Cannot find ckpt from {}.".format(ckpt_path))
-        exit()
+        # exit()
     model.summary(line_length=80)
     cfg['embd_shape'] = m * q
-
 
 
     ##########################################
