5492914dd56e04b1f918412397ef42110a3f31c9
charleslucky <xingbod@gmail.com>
2020-10-06 13:59:07 +0800

    update 1005,distance usage correcting

--- matlab/run_1005_ijbc.m
+++ matlab/run_1005_ijbc.m
@@ -5,11 +5,11 @@
 measure = "Hamming";
 remark = "fusion_learn_DIoMH";
 ds = "IJBC"
-enroll_query_iom_id_fusion("embeddings_dl/ResNet50_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_IJBC_name_dl_256x8.txt",measure,ds,remark)
+enroll_query_iom_id_fusion("embeddings_dl/ResNet50_ijbc_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_ijbc_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_ijbc_name_dl_256x8.txt",measure,ds,remark)
 
-enroll_query_iom_id_fusion("embeddings_dl/ResNet50_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/Xception_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_IJBC_name_dl_256x8.txt",measure,ds,remark)
+enroll_query_iom_id_fusion("embeddings_dl/ResNet50_ijbc_feat_dlIoM_512x8.csv","embeddings_dl/Xception_ijbc_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_ijbc_name_dl_256x8.txt",measure,ds,remark)
 
-enroll_query_iom_id_fusion("embeddings_dl/Xception_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_IJBC_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_IJBC_name_dl_256x8.txt",measure,ds,remark)
+enroll_query_iom_id_fusion("embeddings_dl/Xception_ijbc_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_ijbc_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_ijbc_name_dl_256x8.txt",measure,ds,remark)
 
 run_single("embeddings_0831","Xception","IJBC")
 run_single("embeddings_0831","ResNet50","IJBC")
--- matlab/run_1005_lfw.m
+++ matlab/run_1005_lfw.m
@@ -1,16 +1,16 @@
 % run_single_learning_all("embeddings_dl","Xception","LFW")
-run_single_learning_all("embeddings_dl","ResNet50","LFW")
-run_single_learning_all("embeddings_dl","InceptionResNetV2","LFW")
+% run_single_learning_all("embeddings_dl","ResNet50","LFW")
+% run_single_learning_all("embeddings_dl","InceptionResNetV2","LFW")
 
 
 measure = "Hamming";
 remark = "fusion_learn_DIoMH";
 ds = "LFW"
-enroll_query_iom_id_fusion("embeddings_dl/ResNet50_LFW_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_LFW_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_LFW_name_dl_256x8.txt",measure,ds,remark)
+enroll_query_iom_id_fusion("embeddings_dl/ResNet50_lfw_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_lfw_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_lfw_name_dl_256x8.txt",measure,ds,remark)
 
-enroll_query_iom_id_fusion("embeddings_dl/ResNet50_LFW_feat_dlIoM_512x8.csv","embeddings_dl/Xception_LFW_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_LFW_name_dl_256x8.txt",measure,ds,remark)
+enroll_query_iom_id_fusion("embeddings_dl/ResNet50_lfw_feat_dlIoM_512x8.csv","embeddings_dl/Xception_lfw_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_lfw_name_dl_256x8.txt",measure,ds,remark)
 
-enroll_query_iom_id_fusion("embeddings_dl/Xception_LFW_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_LFW_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_LFW_name_dl_256x8.txt",measure,ds,remark)
+enroll_query_iom_id_fusion("embeddings_dl/Xception_lfw_feat_dlIoM_512x8.csv","embeddings_dl/InceptionResNetV2_lfw_feat_dlIoM_512x8.csv","embeddings_dl/ResNet50_lfw_name_dl_256x8.txt",measure,ds,remark)
 
 
 
