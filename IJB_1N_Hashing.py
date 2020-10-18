4c259e44aef7db355cc67e57ea83d8b2e7157af6
Xingbo <xingbod@gmail.com>
2020-10-18 18:50:58 +0800

    20201018 IJB-C new fusion methold

--- IJB_1N_Hashing.py
+++ IJB_1N_Hashing.py
@@ -191,8 +191,13 @@ def verification(template_norm_feats=None, unique_templates=None, p1=None, p2=No
     for c, s in enumerate(sublists):
         feat1 = template_norm_feats[template2id[p1[s]]]
         feat2 = template_norm_feats[template2id[p2[s]]]
-        similarity_score = np.sum(feat1 * feat2, -1)
+
+        similarity_score = np.sum((feat1 - feat2)**2, -1)# Using euclidean distance to try
+        similarity_score = np.multiply(similarity_score, np.sum((feat1)**2, -1)+np.sum((feat2)**2, -1))
         score[s] = similarity_score.flatten()
+
+        # similarity_score = np.sum(feat1 * feat2, -1)
+        # score[s] = similarity_score.flatten()
         if c % 10 == 0:
             print('Finish {}/{} pairs.'.format(c, total_sublists))
     return score
