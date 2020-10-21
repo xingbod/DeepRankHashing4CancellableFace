37a18933cd3ef10ce58f3a639f26e1aaff50d8ab
Xingbo <xingbod@gmail.com>
2020-10-21 20:45:42 +0800

    20201020 cont. IJBC save to text

--- prepare_IJBC_1N.py
+++ prepare_IJBC_1N.py
@@ -277,7 +277,7 @@ if __name__ == "__main__":
         cfg['m'] = 0
         cfg['q'] = 0
 
-    img_feats = np.load("img_feats_" + cfg['backbone_type'] + '_' + str(is_only_arc) + '_' + str(cfg['m']) + 'x' + str(
+    img_feats = np.load("data_ijbc/img_feats_" + cfg['backbone_type'] + '_' + str(is_only_arc) + '_' + str(cfg['m']) + 'x' + str(
         cfg['q']) + ".npy")
     faceness_scores = np.load("faceness_scores.npy")
     print('img_feats', img_feats.shape)
