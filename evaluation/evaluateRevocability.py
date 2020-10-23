6b72fedafc41d168ba3d2e95d77307740e3321f4
Xingbo <xingbod@gmail.com>
2020-10-23 09:23:06 +0800

    20201020 cont. IJBC save to text

--- evaluation/evaluateRevocability.py
+++ evaluation/evaluateRevocability.py
@@ -67,12 +67,13 @@ if nBins == -1:
 # nonMatedScores = savgol_filter(nonMatedScores, 51, 3) # window size 51, polynomial order 3
 
 # define range of scores to compute D
-bin_edges = numpy.linspace(min([min(matedScores), min(nonMatedScores)]), max([max(matedScores), max(nonMatedScores)]), num=nBins + 1, endpoint=True)
+bin_edges = numpy.linspace(min([min(matedScores), min(nonMatedScores), min(genMatedScores)]), max([max(matedScores), max(nonMatedScores), max(genMatedScores)]), num=nBins + 1, endpoint=True)
 bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2 # find bin centers
 
 # compute score distributions (normalised histogram)
 y1 = numpy.histogram(matedScores, bins = bin_edges, density = True)[0]
 y2 = numpy.histogram(nonMatedScores, bins = bin_edges, density = True)[0]
+y3 = numpy.histogram(genMatedScores, bins = bin_edges, density = True)[0]
 
 # Compute LR and D
 LR = numpy.divide(y1, y2, out=numpy.ones_like(y1), where=y2!=0)
@@ -92,10 +93,12 @@ plt.clf()
 sns.set_context("paper",font_scale=1.7, rc={"lines.linewidth": 2.5})
 sns.set_style("white")
 
-ax = sns.kdeplot(matedScores, shade=False, label='Mated', color=sns.xkcd_rgb["medium green"])
+ax = sns.kdeplot(matedScores, shade=False, label='Mated-imposter', color=sns.xkcd_rgb["medium green"])
 x1,y1 = ax.get_lines()[0].get_data()
-ax = sns.kdeplot(nonMatedScores, shade=False, label='Non-Mated', color=sns.xkcd_rgb["pale red"],linewidth=5, linestyle='--')
+ax = sns.kdeplot(nonMatedScores, shade=False, label='Imposter', color=sns.xkcd_rgb["pale red"],linewidth=5, linestyle='--')
 x2,y2 = ax.get_lines()[1].get_data()
+ax = sns.kdeplot(nonMatedScores, shade=False, label='Genuine', color=sns.xkcd_rgb["black"],linewidth=5, linestyle='*')
+x3,y3 = ax.get_lines()[1].get_data()
 
 ax2 = ax.twinx()
 lns3, = ax2.plot(bin_centers, D, label='$\mathrm{D}_{\leftrightarrow}(s)$', color=sns.xkcd_rgb["denim blue"],linewidth=5)
@@ -115,11 +118,11 @@ ax.set_ylabel("Probability Density")
 ax.set_xlabel("Score")
 ax.set_title("%s, $\mathrm{D}_{\leftrightarrow}^{\mathit{sys}}$ = %.4f" % (figureTitle, Dsys),  y = 1.02)
 
-labs = [ax.get_lines()[0].get_label(), ax.get_lines()[1].get_label(), ax2.get_lines()[0].get_label()]
-lns = [ax.get_lines()[0], ax.get_lines()[1], lns3]
+labs = [ax.get_lines()[0].get_label(), ax.get_lines()[1].get_label(), ax.get_lines()[2].get_label(), ax2.get_lines()[0].get_label()]
+lns = [ax.get_lines()[0], ax.get_lines()[1], ax.get_lines()[2], lns3]
 ax.legend(lns, labs, loc = legendLocation)
 
-ax.set_ylim([0, max(max(y1), max(y2)) * 1.05])
+ax.set_ylim([0, max(max(y1), max(y2), max(y3)) * 1.05])
 ax.set_xlim([bin_edges[0]*0.98, bin_edges[-1]*1.02])
 ax2.set_ylim([0, 1.1])
 ax2.set_ylabel("$\mathrm{D}_{\leftrightarrow}(s)$")
