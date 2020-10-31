e1a54a6069f29c0a1437d2a653a68d1f2ad75534
Xingbo <xingbod@gmail.com>
2020-10-31 20:32:41 +0800

    IJBC

--- IJB_11_Hashing.py
+++ IJB_11_Hashing.py
@@ -211,14 +211,14 @@ def verification(template_norm_feats=None, unique_templates=None, p1=None, p2=No
         # similarity_score = np.sum(feat1 * feat2, -1)
         # score[s] = similarity_score.flatten()
 
-        similarity_score = np.sum((feat1 - feat2)**2, -1)# Using euclidean distance to try
-        similarity_score = np.multiply(similarity_score, np.sum((feat1)**2, -1)+np.sum((feat2)**2, -1))
-        score[s] = similarity_score.flatten()
+        # similarity_score = np.sum((feat1 - feat2)**2, -1)# Using euclidean distance to try
+        # similarity_score = np.multiply(similarity_score, np.sum((feat1)**2, -1)+np.sum((feat2)**2, -1))
+        # score[s] = similarity_score.flatten()
 
 
-        # similarity_score = sklearn.metrics.pairwise_distances(feat1, feat2, metric='euclidean')
-        # similarity_score = similarity_score.flatten()
-        # score[s]  = 1- (similarity_score / ( max(similarity_score)+ 1))
+        similarity_score = sklearn.metrics.pairwise_distances(feat1, feat2, metric='hamming')
+        similarity_score = similarity_score.flatten()
+        score[s]  = 1- (similarity_score / ( max(similarity_score)+ 1))
         if c % 10 == 0:
             print('Finish {}/{} pairs.'.format(c, total_sublists))
     return score
